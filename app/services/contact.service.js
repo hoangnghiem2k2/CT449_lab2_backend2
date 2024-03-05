a089d1a1b50ba701fd56afb8ee26d5702005663e
Ly Hoang Nghiem <hoangnghiem121@gmail.com>
2024-03-05 21:22:43 +0700

    buoc 1

---
+++ app/services/contact.service.js
@@ -0,0 +1,9 @@
+const { ObjectID } = require("mongodb");
+
+class ContactService {
+    constructor(client){
+        this.Contact = client.db().collection("contacts");
+    }
+}
+
+module.exports = ContactService;
